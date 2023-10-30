8cc2d3c8ae4b5862d5bf84a3af840907bf258c85
Marine Van Hees <vanheesmarine@gmail.com>
2023-10-30 17:00:05 +0100

    creation de variables

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-let table1 = document.getElementById("table1").innerHTML
-let offences = table1.getElementsByTagName("td").innerHTML
+let table1 = document.getElementById("table1").innerHTML;
+let offences = table1.getElementsByTagName("td").innerHTML;
 
-let table2 = document.getElementById("table2").innerHTML
-let prisonpopulation = table2.getElementsByTagName("td").innerHTML
+let table2 = document.getElementById("table2").innerHTML;
+let prisonpopulation = table2.getElementsByTagName("td").innerHTML;
 
+const tables = document.querySelectorAll("table");
+
+const canvas = document.createElement("canvas");
+canvas.id = "chart${index}";
+canvas.width = 400;
+canvas.height = 200;
+
+
+table1.parentNode.insertBefore(canvas, table1);
