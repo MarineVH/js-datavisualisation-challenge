5b4c591e9769976c5bd449c7ca4960fb7f610d58
Marine Van Hees <vanheesmarine@gmail.com>
2023-10-30 15:34:26 +0100

    creation variable

---
+++ main.js
@@ -0,0 +1,6 @@
+let table1 = document.getElementById("table1").innerHTML
+let offences = table1.getElementsByTagName("td").innerHTML
+
+let table2 = document.getElementById("table2").innerHTML
+let prisonpopulation = table2.getElementsByTagName("td").innerHTML
+
