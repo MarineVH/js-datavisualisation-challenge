d2e8407354c5a0fd99ae86e3fc5e27da953005a3
Marine Van Hees <vanheesmarine@gmail.com>
2023-10-31 16:23:53 +0100

    test

--- main.js
+++ main.js
@@ -6,10 +6,42 @@ let prisonpopulation = table2.getElementsByTagName("td").innerHTML;
 
 const tables = document.querySelectorAll("table");
 
-const canvas = document.createElement("canvas");
-canvas.id = "chart${index}";
-canvas.width = 400;
-canvas.height = 200;
+tables.forEach((table, index) => {
+    const canvas = document.createElement("canvas");
+    canvas.id = "chart${index}";
+    canvas.width = 400;
+    canvas.height = 200;
 
+    table.parentNode.insertBefore(canvas, table);
 
-table1.parentNode.insertBefore(canvas, table1);
+    const labels = [];
+    const data = [];
+    const rows = table.querySelectorAll("tr");
+
+    rows.forEach((row, rowIndex) => {
+        if (rowIndex === 0) {
+            return;
+        }
+
+        const cells = row.querySelectorAll("td");
+        labels.push(cells[0].textContent);
+        data.push(Number(cells[1].textContent));
+    });
+
+    new Chart(canvas, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Chart.js Line Chart'
+                }
+            }
+        },
+    });
+})
