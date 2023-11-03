2c5f98acd10d0d8859865bcb8ff957055b2e17ce
Marine Van Hees <vanheesmarine@gmail.com>
2023-11-03 15:59:10 +0100

    2nd graph

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ let offences = table1.getElementsByTagName("td");
 let table2 = document.getElementById("table2");
 let prisonpopulation = table2.getElementsByTagName("td");
 
-const canvas = document.createElement("canvas");
+const canvas1 = document.createElement("canvas");
+const canvas2 = document.createElement("canvas");
 
-table1.parentNode.insertBefore(canvas, table1);
+table1.parentNode.insertBefore(canvas1, table1);
+table2.parentNode.insertBefore(canvas2, table2);
 
-new Chart(canvas, {
+new Chart(canvas1, {
     type: 'line',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -25,4 +27,23 @@ new Chart(canvas, {
         }
       }
     }
+});
+
+new Chart(canvas2, {
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
 });
