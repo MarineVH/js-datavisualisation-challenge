f78644df5e75df4b897e362a0b9b585250b67ebb
Marine Van Hees <vanheesmarine@gmail.com>
2023-11-08 16:12:43 +0100

    recommencer

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-
 const table1 = document.getElementById("table1");
 let canvas = document.createElement("canvas");
-canvas.id = "canvas1";
 
 table1.parentNode.insertBefore(canvas, table1);
 
-const row1 = table1.querySelectorAll("tr:first-child");
-
-const years = [];
-
-for (let i = 0; i < row1.child.length; i++) {
-  const cell = row1.children[i];
-  const cellContent = cell.innerHTML.trim();
-
-  const year = parseInt(cellContent);
-  if (!isNaN(year)) {
-    years.push(year);
-  }
-}
-
-console.log(years);
+let years1 = [];
+let country1 = [];
+let offenses =[];
 
 new Chart(canvas, {
     type: 'line',
     data: {
-      labels: labels,
+      labels: years,
       datasets: [{
-        label: '# of Votes',
-        data: data,
+        label: country,
+        data: offenses,
         borderWidth: 1
       }]
     },
@@ -38,32 +24,4 @@ new Chart(canvas, {
         }
       }
     }
-});
-
-
-const table2 = document.getElementById("table2");
-let newCanvas = document.createElement("canvas");
-newCanvas.id = "canvas2";
-
-table2.parentNode.insertBefore(canvas2, table2);
-
-const rows2 = table2.querySelectorAll("tr");
-
-new Chart(canvas2, {
-  type: 'line',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
 });
